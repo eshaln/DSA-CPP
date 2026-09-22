097ba88eb8738b3eea07a315391dc1d71683f6bb
Eshal <eshalnaeem2008@gmail.com>
2026-09-22 19:10:38 +0500

    Add array practice program

---
+++ 01_Arrays/AccessElements.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string cars[5]={"BMW","MAZDA","FORD","VOLVO","TOYOTA"};
+
+    for (int i=0;i<5;i++) {
+        cout<<cars[i]<<endl;
+
+    }
+    return 0;
+}
